b91229c8f96b56792657bbd7144ba69e4eaae61a
kein <kein10119810@gmail.com>
2025-03-05 22:29:45 +0900

    DBConnector

--- TestDB/src/DBConnector.java
+++ TestDB/src/DBConnector.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 public class DBConnector {
 	private static String driverName ="com.mysql.jdbc.Driver";
-	private static String url ="jdbc:mysql://localhost/testtable?autoReconnect=true&useSSL=false";
+	private static String url ="jdbc:mysql://localhost/testdb?autoReconnect=true&useSSL=false";
 
 	private static String user ="root";
 	private static String password ="";
